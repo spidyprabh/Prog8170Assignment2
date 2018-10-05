1c7391a0fadf6c4b237a848b150f3ce2f5e8bf2e
prabhjyot <psingh1402@conestogac.on.ca>
2018-10-05 11:23:24 -0400

    First Commit

---
+++ Prog8170Assignment2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prog8170Assignment2
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Triangle p = new triangle;
+      int choice, length, brea
+
+    }
+  }
+}
